ffb9bc5729172bc55814b4b65487280c5d8c8c2f
r-ILIASS <iliass.root@gmail.com>
2022-07-16 19:36:00 +0100

    add base enemy class and draw some enemy skeletons

--- script.js
+++ script.js
@@ -96,7 +96,31 @@ window.addEventListener("load", () => {
     }
   }
 
-  class Enemey {}
+  class Enemy {
+    constructor(game) {
+      this.game = game;
+      this.x = this.game.width;
+      this.speedX = Math.random() * -1.5 - 0.5;
+      this.markedForDeletion = false;
+    }
+    update() {
+      this.x += this.speedX;
+      if (this.x + this.width < 0) this.markedForDeletion = true;
+    }
+    draw(context) {
+      context.fillStyle = "red";
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  class Angler1 extends Enemy {
+    constructor(game) {
+      super(game);
+      this.width = 228 * 0.2;
+      this.height = 169 * 0.2;
+      this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    }
+  }
 
   class Layer {}
 
@@ -121,15 +145,19 @@ window.addEventListener("load", () => {
     // prettier-ignore
     constructor(canvasWidth, canvasHeight) {
       this.width = canvasWidth;                // match the canvas width
-      this.heigth = canvasHeight;              // match the canvas height
+      this.height = canvasHeight;              // match the canvas height
       this.player = new Player(this);          // new instance of Player
       this.input = new InputHandler(this);     // new instance of InputHandler
       this.UI = new UI(this);                  // new instance of UI
       this.keys = [];                          // keep track of pressed keys
+      this.enemies = [];                       // all active enemies
+      this.enemyTimer = 0;                     // in ms
+      this.enemyInterval = 1000;               // in ms
       this.ammo = 20;                          
       this.maxAmmo = 50;
       this.ammoTimer = 0;                      // in ms
       this.ammoInterval = 500;                 // in ms
+      this.gameOver = false;                   // game over
     }
     update(deltaTime) {
       this.player.update();
@@ -141,14 +169,28 @@ window.addEventListener("load", () => {
       } else {
         this.ammoTimer += deltaTime;
       }
+      this.enemies.forEach((enemy) => {
+        enemy.update();
+      });
+      this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
+      if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
     }
     draw(context) {
       this.player.draw(context);
       this.UI.draw(context);
+      this.enemies.forEach((enemy) => enemy.draw(context));
+    }
+    addEnemy() {
+      this.enemies.push(new Angler1(this));
     }
   }
 
-  const game = new Game(canvas.width, canvas.heigth);
+  const game = new Game(canvas.width, canvas.height);
   let lastTime = 0;
   // animation loop
   function animate(timeStamp) {
