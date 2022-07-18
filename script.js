a389be8c38826df1bca6825e9ec823a1dbf4d324
r-ILIASS <iliass.root@gmail.com>
2022-07-18 13:00:32 +0100

    player sprite animation

--- script.js
+++ script.js
@@ -59,9 +59,13 @@ window.addEventListener("load", () => {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
       this.speedY = 0;
-      this.maxSpeed = 5;
+      this.maxSpeed = 3;
       this.projectiles = [];
+      this.image = document.getElementById("player");
     }
     update() {
       // handle mouvement
@@ -70,7 +74,6 @@ window.addEventListener("load", () => {
         this.speedY = this.maxSpeed;
       else this.speedY = 0;
       this.y += this.speedY;
-
       // handle projectiles
       this.projectiles.forEach((projectile) => {
         projectile.update();
@@ -78,10 +81,25 @@ window.addEventListener("load", () => {
       this.projectiles = this.projectiles.filter(
         (projectile) => !projectile.markedForDeletion
       );
+      // sprite animation logic
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
     }
     draw(context) {
-      context.fillStyle = "green";
-      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       this.projectiles.forEach((projectile) => {
         projectile.draw(context);
       });
